9c1f30f5a317a559c9d132a54dbd9faf0f18bcf8
Ben Mitchell <ben.mitchell@instantonline.nz>
2022-08-27 23:26:27 +1200

    require userid to be correct before delete or update

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -76,6 +76,10 @@ class ListingController extends Controller
      */
     public function update(Listing $listing)
     {
+        if ($listing->user_id != auth()->id()) {
+            abort(403, 'Unauthorized Action');
+        }
+
 
         $formFields = request()->validate([
             'title' => 'required',
@@ -102,6 +106,10 @@ class ListingController extends Controller
      */
     public function destroy(Listing $listing)
     {
+        if ($listing->user_id != auth()->id()) {
+            abort(403, 'Unauthorized Action');
+        }
+
         $listing->delete();
 
         return redirect('/')->with('message', 'Deleted successfully.');
