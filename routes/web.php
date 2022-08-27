8ffcb696220d07a84a25c42246c16680216a8a3d
Ben Mitchell <ben.mitchell@instantonline.nz>
2022-08-27 21:48:40 +1200

    Add edit functionality

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,12 @@ Route::get('/listings/create', [ListingController::class, 'create']);
 // Store listing data.
 Route::post('/listings', [ListingController::class, 'store']);
 
+// Get edit form.
+Route::get('/listings/{listing}/edit', [ListingController::class, 'edit']);
+
+// Update listing
+Route::put('/listings/{listing}', [ListingController::class, 'update']);
+
 // Single listing.
 Route::get('/listings/{listing}', [ListingController::class, 'show']);
 
